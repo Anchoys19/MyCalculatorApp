f975b5dbb5907dd83f9c5792561a5ea3c4e38d3e
Anchoys19 <anna873637@gmail.com>
2023-06-23 23:35:15 +0300

    Initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MyCalculatorApp 
+{
+    internal class Program
+    {
+        private static double result;
+
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            char operation;
+            Console.WriteLine("Hello person");
+            Console.Write("Enter tne first number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a sign(+,-,*,/): ");
+            operation = Convert.ToChar(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            double resolt = 0;
+            Console.Write("Enter tne result: ");
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Wrong operation");
+                    break;
+            }
+            Console.WriteLine("Результат: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
