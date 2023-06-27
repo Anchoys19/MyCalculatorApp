d00babae73bff5c70a8dd66f6dfcc54b6da40bfa
Anchoys19 <anna873637@gmail.com>
2023-06-27 16:44:52 +0300

    Test

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,44 +1,51 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
-namespace MyCalculatorApp 
+namespace MyCalculatorApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double num1, num2;
-            char operation;
-            Console.WriteLine("Hello person");
-            Console.Write("Enter tne first number: ");
-            num1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter a sign(+,-,*,/): ");
-            operation = Convert.ToChar(Console.ReadLine());
-            Console.Write("Enter the second number: ");
-            num2 = Convert.ToDouble(Console.ReadLine());
-            double result = 0;
-            Console.Write("Enter tne result: ");
-            switch (operation)
+            try
             {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    break;
-                default:
-                    Console.WriteLine("Wrong operation");
-                    break;
+                Console.WriteLine("Hello person");
+                Console.Write("Enter tne first number: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter a sign(+,-,*,/): ");
+                char operation = Convert.ToChar(Console.ReadLine());
+                Console.Write("Enter the second number: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+                double result = 0;
+                Console.Write("Enter tne result: ");
+                switch (operation)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        result = num1 / num2;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong operation");
+                        break;
+                }
+                Console.WriteLine(result);
+                Console.ReadLine();
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
             }
-            Console.WriteLine("Результат: " + result);
-
-            Console.ReadKey();
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number format. Please enter a valid number.");
+            }
         }
     }
 }
+
