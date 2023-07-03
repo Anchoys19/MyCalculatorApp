8619dcc6af5400f7d02058741bf9567e80c9b637
Anchoys19 <anna873637@gmail.com>
2023-07-03 11:47:30 +0300

    update program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System;
+using System;
 using System.Runtime.CompilerServices;
 
 namespace MyCalculatorApp
@@ -7,45 +9,47 @@ namespace MyCalculatorApp
     {
         static void Main(string[] args)
         {
-            try
+            while (true)
             {
-                Console.WriteLine("Hello person");
-                Console.Write("Enter tne first number: ");
-                double num1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Enter a sign(+,-,*,/): ");
-                char operation = Convert.ToChar(Console.ReadLine());
-                Console.Write("Enter the second number: ");
-                double num2 = Convert.ToDouble(Console.ReadLine());
-                double result = 0;
-                Console.Write("Enter tne result: ");
-                switch (operation)
+                try
                 {
-                    case '+':
-                        result = num1 + num2;
-                        break;
-                    case '-':
-                        result = num1 - num2;
-                        break;
-                    case '*':
-                        result = num1 * num2;
-                        break;
-                    case '/':
-                        result = num1 / num2;
-                        break;
-                    default:
-                        Console.WriteLine("Wrong operation");
-                        break;
+                    Console.WriteLine("Hello person");
+                    Console.Write("Enter tne first number: ");
+                    double num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter a sign(+,-,*,/): ");
+                    char operation = Convert.ToChar(Console.ReadLine());
+                    Console.Write("Enter the second number: ");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+                    double result = 0;
+                    Console.Write("Result is: ");
+                    switch (operation)
+                    {
+                        case '+':
+                            result = num1 + num2;
+                            break;
+                        case '-':
+                            result = num1 - num2;
+                            break;
+                        case '*':
+                            result = num1 * num2;
+                            break;
+                        case '/':
+                            result = num1 / num2;
+                            break;
+                        default:
+                            Console.WriteLine("Wrong operation");
+                            break;
+                    }
+                    Console.WriteLine(result);
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey();
+                    break;
                 }
-                Console.WriteLine(result);
-                Console.ReadLine();
-                Console.WriteLine("Press any key to continue");
-                Console.ReadKey();
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Invalid number format. Please enter a valid number.");
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid number format. Please enter a valid number.");
+                }
             }
         }
     }
 }
-
